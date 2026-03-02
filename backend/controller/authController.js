5236501b2ba9b7878c97ef94c3f0c30af43b20be
sanjana-bh <sanjanabharadwaj060505@gmail.com>
2026-03-02 21:14:02 +0530

    Update authController.js

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -28,8 +28,8 @@ export const signUp = async (req,res) => {
         
         res.cookie("token",token , {
             httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
        return res.status(201).json(user)
@@ -53,8 +53,8 @@ export const login = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token , {
             httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
        return res.status(200).json(user)
@@ -151,8 +151,8 @@ export const googleAuth = async (req,res) => {
         let token = await genToken(user._id)
         res.cookie("token",token , {
             httpOnly:true,
-            secure:false,
-            sameSite: "Strict",
+            secure:true,
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
        return res.status(200).json(user)
