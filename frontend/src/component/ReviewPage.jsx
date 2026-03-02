6d4ec622441c778bcd045aa006c430c14e58444d
sanjana-bh <sanjanabharadwaj060505@gmail.com>
2026-03-02 21:26:56 +0530

    Update ReviewPage.jsx

--- frontend/src/component/ReviewPage.jsx
+++ frontend/src/component/ReviewPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
-import ReviewCard from './reviewCard'
+import ReviewCard from './ReviewCard'
 
 function ReviewPage() {
    const {reviewData} = useSelector(state=>state.review)
